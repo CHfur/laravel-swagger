fbc810ebf89f11b725938892427224ef753b0027
CHfur <forfoxford@mail.ru>
2022-02-14 16:52:44 +0300

    Add web middleware to all routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use RonasIT\Support\AutoDoc\Http\Controllers\AutoDocController;
 
-Route::get('/auto-doc/documentation', ['uses' => AutoDocController::class.'@documentation']);
-Route::get('/auto-doc/{file}', ['uses' => AutoDocController::class.'@getFile']);
-foreach (config('auto-doc.routes') as $route) {
-    Route::middleware(config('auto-doc.middlewares'))->get($route, ['uses' => AutoDocController::class.'@index']);
-}
+Route::middleware('web')->group(function () {
+    Route::get('/auto-doc/documentation', ['uses' => AutoDocController::class . '@documentation']);
+    Route::get('/auto-doc/{file}', ['uses' => AutoDocController::class . '@getFile']);
+    foreach (config('auto-doc.routes') as $route) {
+        Route::middleware(config('auto-doc.middlewares'))->get($route, ['uses' => AutoDocController::class . '@index']);
+    }
+});
